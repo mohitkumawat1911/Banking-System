4fb644164abe20f80c0b865eb5b5291e8f4e7070
mohitkumawat1911 <51379899+mohitkumawat1911@users.noreply.github.com>
2020-10-18 09:55:36 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home </title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <style type="text/css">
+    .logo-text{
+	  font-size:50px;
+	  padding-left: 45%;
+      color: white;
+      padding-top: 20px;
+	  
+    }
+    
+    .main-img{
+      width:100%;
+      margin: 0;
+    }
+    .main-text{
+      text-align: center;
+      font-size: 40px;
+    }
+    .p2{
+      font-size: 30px;
+    }
+    .button1 {
+      background-color: black;
+      color: white;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+      
+      font-size: 20px;
+      margin: 4px 2px;
+      border-radius: 60px;
+    }
+	.button2 {
+      background-color: black;
+      border: none;
+      color: white;
+      padding: 15px 32px;
+      text-align: center;
+      text-decoration: none;
+     
+      font-size: 20px;
+      margin: 10px 10px;
+      border-radius: 60px;
+    }
+    
+	.view-btn2{
+      text-align:center ;
+    }
+	.mo{
+		 background-color: orange;
+		  padding: 50px 50px;
+		   margin: 10% 20%;
+	}
+	
+    </style>
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+ 
+  <p class="logo-text">White Bank</p>
+  </nav>
+  
+ <div class="mo" >
+ <div class="view-btn2">
+     <a href="adduser.php" class="button1"> Add Customers</a>
+  </div>
+  <br></br>
+  <div class="view-btn2">
+     <a href="viewusers.php" class="button1"> All Customers</a>
+  </div>
+  <br></br>
+   
+  <div class="view-btn2">
+     <a href="transactionDetails.php" class="button2">Transaction History</a>
+  </div>
+  </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  
+  
+  
+   <footer class="footer1" align="center"> <br>
+    <div  style="height: 60%">
+    <p>  Designed by Mohit Kumawat </p>
+ 
+    </div>
+  </footer>
+  
+  
+  
+  </body>
+</html>
